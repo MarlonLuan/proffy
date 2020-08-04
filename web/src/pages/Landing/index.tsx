15054fe2513b267dcafe4aa9cff577aa8dbf426a
Marlon Luan <marlonluan@gmail.com>
2020-08-04 07:45:06 -0300

    Icones

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 
+import studyIcon from '../../assets/images/icons/study.svg'
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHearthIcon from '../../assets/images/icons/purple-heart.svg'
+
 function Landing() {
   return (
     <div id="page-landing">
@@ -17,6 +21,25 @@ function Landing() {
                 alt="Plataforma de Estudos"
                 className="hero-image"
             />
+
+            <div className="buttons-container">
+                <a href="" className="study">
+                    <img src={studyIcon} alt="Estudar" />
+                    Estudar
+                </a>
+            </div>
+
+            <div className="give-classes">
+                <a href="" className="study">
+                    <img src={giveClassesIcon} alt="Dar aulas" />
+                    Dar aulas
+                </a>
+            </div>
+
+            <span className="total-connections">
+                Total de 200 conexões já realizadas
+                <img src={purpleHearthIcon} alt="Coração Roxo" />
+            </span>
         </div>
     </div>
   )
