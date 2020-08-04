2ebd0b5536bc650feb4206f5c01b416fb242a4f1
Marlon Luan <marlonluan@gmail.com>
2020-08-04 09:04:59 -0300

    TeacherList Index

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,28 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
+import logoImg from '../../assets/images/logo.svg'
+import backIcon from '../../assets/images/icons/back.svg'
 
 function TeacherList() {
     return (
-        <h1>Teacher List</h1>
+       <div className="container" id="page-teacher-list">
+         <header className="page-header">
+             <div className="top-bar-container">
+                 <Link to="/">
+                    <img src={backIcon} alt="Voltar" />
+                    Voltar
+                 </Link>
+                 <img src={logoImg} alt="Proffy" />
+             </div>
+
+             <div className="header-content">
+                 <strong>
+                     Esses são os proffys disponíveis
+                 </strong>
+             </div>
+         </header>
+       </div>
     )    
 }
 
