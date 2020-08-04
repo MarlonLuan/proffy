630fdc796c4db30a7923223a6046c620fb272a4f
Marlon Luan <marlonluan@gmail.com>
2020-08-04 07:36:19 -0300

    Landing Img

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg'
+import landingImg from '../../assets/images/landing.svg'
 
 function Landing() {
   return (
@@ -8,7 +9,14 @@ function Landing() {
         <div id="page-landing-content" className="container">
             <div className="logo-container">
                 <img src={logoImg} alt="Proffy"/>
+                <h2>Sua plataforma de estudos online.</h2>
             </div>
+
+            <img
+                src={landingImg}
+                alt="Plataforma de Estudos"
+                className="hero-image"
+            />
         </div>
     </div>
   )
